5909e9649cabf7ac25bb8b7e90a0b04a06ce720e
joacoleporta <joaquinleporta@gmail.com>
2023-12-05 15:02:34 +0100

    home audio

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
-/*document.getElementById('miBoton').onclick = function () {
-    var miAudio = document.getElementById('audio');
+// Obtén todos los elementos con la clase btnred y btnblue
+const buttons = document.querySelectorAll('.btnred, .btnblue');
+
+// Itera sobre cada botón
+buttons.forEach(button => {
+  // Agrega un evento de clic a cada botón
+  button.addEventListener('click', function () {
+    // Obtén la ruta del audio del atributo data-audio
+    const audioPath = this.getAttribute('data-audio');
+
+    // Crea un nuevo elemento de audio
+    const audio = new Audio(audioPath);
+
+    // Reproduce el audio
     audio.play();
-    window.location.href = ""
-};
+  });
+});
+
